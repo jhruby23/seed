e58b2885069bcd375f0bd1f93e372f3be684767a
Honza <jhruby@meds.cz>
2016-02-10 16:57:28 +0100

    Fix

--- config/app.php
+++ config/app.php
@@ -148,7 +148,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         
-        Illuminate\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -204,8 +204,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         
-        'Form'		  => Illuminate\Html\FormFacade::class,
-        'Html'		  => Illuminate\Html\HtmlFacade::class,
+        'Form'		  => Collective\Html\FormFacade::class,
+        'Html'		  => Collective\Html\HtmlFacade::class,
 
     ],
 
